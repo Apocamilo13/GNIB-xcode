1bae02cd4e0f8db1a9ac66bf1d937130fadd7620
Anderson Soares Rodrigues <srodrigues.anderson@gmail.com>
2018-01-10 11:55:31 +0000

    Initial Commit

---
+++ GNIB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GNIB
+//
+//  Created by Anderson Soares Rodrigues on 10/01/2018.
+//  Copyright © 2018 Anderson Soares Rodrigues. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
